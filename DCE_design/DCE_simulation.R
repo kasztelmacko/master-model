bee51891fe51562b08be779d58910ded181f59f8
kasztelmacko <m.kasztelani@student.uw.edu.pl>
2025-07-05 13:53:05 +0200

    full design and simulations

--- DCE_design/DCE_simulation.R
+++ DCE_design/DCE_simulation.R
@@ -7,6 +7,7 @@ library(gmnl)
 library(mlogit)
 
 design <- read.csv("data/DCE_design_full.csv", stringsAsFactors = FALSE)
+design <- read.csv("data/DCE_design.csv", stringsAsFactors = FALSE)
 set.seed(123)
 
 simulation <- cbc_choices(
@@ -62,6 +63,7 @@ simplest_mnl <- gmnl(
 )
 summary(simplest_mnl)
 
+#full
 # Coefficients:
 #                      Estimate Std. Error z-value  Pr(>|z|)
 # brand_burger_king   -0.278535   0.200580 -1.3886  0.164941
@@ -72,6 +74,18 @@ summary(simplest_mnl)
 # type_bundle_premium  0.709796   0.596894  1.1892  0.234381
 # price               -0.056316   0.033337 -1.6893  0.091165 .
 # no_choice            2.033531   0.504724  4.0290 5.602e-05 ***
+
+#old dopt
+# Coefficients:
+#                      Estimate Std. Error z-value  Pr(>|z|)
+# brand_burger_king    0.030810   0.278232  0.1107  0.911826
+# brand_max_burger    -0.161096   0.280071 -0.5752  0.565159
+# brand_wendys        -0.196316   0.281665 -0.6970  0.485812
+# type_burger_premium -1.108154   0.391227 -2.8325  0.004618 **
+# type_bundle_classic -0.503076   0.375446 -1.3399  0.180264    
+# type_bundle_premium -0.845883   0.494066 -1.7121  0.086881 .
+# price                0.012635   0.025420  0.4971  0.619139
+# no_choice            3.085211   0.464659  6.6397 3.142e-11 ***
 
 simplest_mixl <- gmnl(
     choice ~ 
@@ -102,6 +116,7 @@ simplest_mixl <- gmnl(
 )
 summary(simplest_mixl)
 
+#full
 # Coefficients:
 #                         Estimate Std. Error z-value Pr(>|z|)
 # no_choice               1.024138   1.245930  0.8220  0.41108  
@@ -120,6 +135,25 @@ summary(simplest_mixl)
 # sd.type_bundle_premium  3.427291   2.365406  1.4489  0.14736
 # sd.price                0.098401   0.078634  1.2514  0.21080
 
+#old dopt
+# Coefficients:
+#                          Estimate Std. Error z-value Pr(>|z|)
+# no_choice                6.740875   5.812545  1.1597   0.2462
+# brand_burger_king      -17.273405  14.993504 -1.1521   0.2493
+# brand_max_burger        -1.868189   2.438560 -0.7661   0.4436
+# brand_wendys           -73.954083  60.266294 -1.2271   0.2198
+# type_burger_premium    -80.685503  65.013128 -1.2411   0.2146
+# type_bundle_classic     -9.015680   7.584253 -1.1887   0.2345
+# type_bundle_premium    -10.865510   9.032751 -1.2029   0.2290
+# price                   -0.088963   0.133891 -0.6644   0.5064
+# sd.brand_burger_king    16.632074  13.826060  1.2030   0.2290
+# sd.brand_max_burger      3.552512   3.809423  0.9326   0.3510
+# sd.brand_wendys         48.698010  39.270278  1.2401   0.2149
+# sd.type_burger_premium  48.759981  39.200037  1.2439   0.2135
+# sd.type_bundle_classic   8.810985   7.138731  1.2343   0.2171
+# sd.type_bundle_premium   8.983059   7.270925  1.2355   0.2167
+# sd.price                 0.242547   0.207661  1.1680   0.2428
+
 processed_mnl <- gmnl(
     choice ~ 
     is_well_known +
@@ -136,6 +170,7 @@ processed_mnl <- gmnl(
 )
 summary(processed_mnl)
 
+#full
 # Coefficients:
 #                Estimate Std. Error z-value  Pr(>|z|)
 # is_well_known  0.319217   0.151586  2.1058   0.03522 *
@@ -143,6 +178,15 @@ summary(processed_mnl)
 # is_premium     0.326395   0.320278  1.0191   0.30816
 # price         -0.060187   0.031773 -1.8943   0.05818 .
 # no_choice      2.443364   0.486803  5.0192 5.189e-07 ***
+
+#old dopt
+# Coefficients:
+#                 Estimate Std. Error z-value  Pr(>|z|)
+# is_well_known  0.1660622  0.1979014  0.8391   0.40140
+# is_bundle     -0.1260098  0.3123779 -0.4034   0.68666
+# is_premium    -0.6275751  0.2767987 -2.2673   0.02337 *  
+# price          0.0045993  0.0255017  0.1804   0.85687
+# no_choice      3.2542124  0.4553617  7.1464 8.906e-13 ***
 
 processed_mixl <- gmnl(
     choice ~ 
@@ -167,6 +211,7 @@ processed_mixl <- gmnl(
 )
 summary(processed_mixl)
 
+#full
 # Coefficients:
 #                   Estimate Std. Error z-value Pr(>|z|)
 # no_choice        -1.072350   1.867549 -0.5742  0.56583
@@ -178,3 +223,16 @@ summary(processed_mixl)
 # sd.is_bundle      2.676682   1.545432  1.7320  0.08327 .
 # sd.is_premium     0.046175   1.072358  0.0431  0.96565
 # sd.price          0.285164   0.172891  1.6494  0.09907 .
+
+#old dopt
+# Coefficients:
+#                   Estimate Std. Error z-value Pr(>|z|)
+# no_choice         2.457314   1.685836  1.4576   0.1449
+# is_well_known     0.163288   0.288148  0.5667   0.5709
+# is_bundle        -0.202534   0.677629 -0.2989   0.7650
+# is_premium       -1.554545   1.754227 -0.8862   0.3755
+# price            -0.070718   0.170605 -0.4145   0.6785
+# sd.is_well_known  0.160315   1.694901  0.0946   0.9246
+# sd.is_bundle      0.758821   1.167076  0.6502   0.5156
+# sd.is_premium     1.637498   1.769304  0.9255   0.3547
+# sd.price          0.068915   0.095123  0.7245   0.4688
